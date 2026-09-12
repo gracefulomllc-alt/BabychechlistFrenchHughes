561dd804da7888f48cd14de37241e6cc786d9c9d
Anthony <anthony@homeawayinn.com>
2026-09-12 06:59:57 +0000

    v16 name editing, mobile tab dropdown

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Minimal service worker: caches the shell so the app opens offline; API calls always go to the network.
-const C="baby-prep-v13";
+const C="baby-prep-v16";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["/","/party.js","/ram.svg","/weeks.js","/after.js","/prices.js","/manifest.json","/icon-192.png"])));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));});
 self.addEventListener("fetch",e=>{
